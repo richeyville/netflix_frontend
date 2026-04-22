ed9ca2f39de39e93f938fbf6038d1feb5484e8ef
richeyville <richeyville@outlook.com>
2026-04-22 15:55:41 +0100

    Update axios base URL for API requests

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL:'http://3.15.194.21:8080',
+    baseURL:'http://3.139.87.109:8080',
     headers: {
         'Content-Type': 'application/json',
     },
