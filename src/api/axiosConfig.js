b6a595438b929c94328e34353a57cccc7b739823
richeyville <richeyville@outlook.com>
2026-04-28 20:34:10 +0100

    Update axiosConfig.js

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL:'http://a3162dce05f934e1da8447a9d4182761-1353929318.us-east-1.elb.amazonaws.com:8080',
+    baseURL:'http://a5a71ad7ba0ee499295a88aa2e16f50d-1754491642.us-east-1.elb.amazonaws.com:8080',
     headers: {
         'Content-Type': 'application/json',
     },
